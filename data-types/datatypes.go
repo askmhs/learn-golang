e59c7c2463723afc575559e6e851e5cd65951c7f
Muhammad Harits Syaifulloh <muhammadharits@icloud.com>
2020-09-26 10:26:17 +0700

    Data types

---
+++ data-types/datatypes.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// String types
+	fmt.Println("This is string")
+
+	// Number types
+	fmt.Println("This is number")
+	fmt.Println(1)
+	fmt.Println(-1)
+	fmt.Println(3.5)
+
+	// Boolean
+	fmt.Println("This is boolean")
+	fmt.Println(true)
+	fmt.Println(false)
+}
